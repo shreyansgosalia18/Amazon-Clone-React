765c36f3a198f3075be4d53416f359cb0d04bf3c
Shreyans Gosalia <shreyansgosalia18@gmail.com>
2022-07-17 16:56:34 +0530

    UI work with API call

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5ntmUdmOKd56HqYlxOY5K43co4AtwQiI",
+  authDomain: "clone-react-3413a.firebaseapp.com",
+  projectId: "clone-react-3413a",
+  storageBucket: "clone-react-3413a.appspot.com",
+  messagingSenderId: "1065950342491",
+  appId: "1:1065950342491:web:67676772bfded880263e7e"
+};
